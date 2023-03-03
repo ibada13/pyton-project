128267eba000314c73334c9b396e8ec7b9aa8b04
hunter morningstar <87934797+hunter666hz@users.noreply.github.com>
2023-03-03 20:49:06 +0100

    Add files via upload

---
+++ sec.py
@@ -0,0 +1,31 @@
+
+
+
+name = input("enter your name : ")
+find =False
+pos = None
+
+
+
+with open("start.txt","r") as e :
+        ar = e.read().split(" ")
+        x = len(ar)
+for i in range(x):
+    if(name == ar[i] ): 
+        find = True
+        pos = i+1
+        break
+
+
+if find : ##i>x
+     print("the name exist in the file the postion of your name is ",pos)
+else :
+        add = input("\n 1 - if you want to add the mame to the list enter 1 \n 2 - else enter  anything else 1 \n");
+        if(add=="1"): 
+             with open("start.txt","a") as e :
+                   e.write(" "+name)
+                   e.close()
+                   print("done adding")
+        else:
+             print("done")    
+              
